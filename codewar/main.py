939b15957bb689b9d34fec9dd29a5ba27acc3369
ryan <39787691+parven-dev@users.noreply.github.com>
2024-09-24 16:59:34 +0530

    higher lower str to num

--- codewar/main.py
+++ codewar/main.py
@@ -1,14 +1,12 @@
-def explode(arr):  
-    array = []
-    for item in arr:
-        if type(item) == int:
-            for _ in range(item):
-                array.append(arr)
-                
-        if all(isinstance(x, str) for x in arr):
-            return "Void!"
-            
-    return array
-arr = ["f", "r"]
-print(explode(arr))
+def high_and_low(numbers: str):
+  split_num = numbers.split(" ")
+  num = [int(items) for items in split_num ]
+  max_num =  str(max(num))
+  min_num = str(min(num))
+  combine_num = max_num + " " + min_num
+  return combine_num
+  
+ 
 
+numbers = ("8 3 -5 42 -1 0 0 -9 4 7 4 -4")
+print(high_and_low(numbers=numbers))
