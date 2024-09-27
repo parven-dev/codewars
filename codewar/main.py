a01d4ee7c864fe2855bf9de64e9958370fc7cec7
ryan <39787691+parven-dev@users.noreply.github.com>
2024-09-27 17:19:27 +0530

    consecutive array

--- codewar/main.py
+++ codewar/main.py
@@ -1,11 +1,23 @@
-def get_count(sentence):
-    get_vowl = ''
-    for vowl in sentance:
-        if vowl in "aeiou":
-            get_vowl+=vowl
-            
-    return len(get_vowl)
-sentance  = "abracadabra"
-print(get_count(sentence=sentance))
+def longest_consec(strarr, k):
+    
+    string_length = ""
+    if k < len(strarr) and k > 0:
+        for string in range(len(strarr)):
+            string_added = strarr[string:string+k]
+            if len(string_added) == k:
+                strin = "".join(string_added)
+                string_length+=strin + ","
+    if k  == 0 or k > len(strarr) or k < 0:
+        return ""
+    
+    
+    split_str = string_length.split(',')
+    len_stinng = []
+    for item in split_str:
+        len_stinng.append(len(item))
+    return len_stinng
 
+starr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"]
+k = 0
 
+print(longest_consec(starr, k))
