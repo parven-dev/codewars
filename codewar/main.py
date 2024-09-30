5fc9cd14c493c9df6e6deac05da59b410afc91ad
ryan <39787691+parven-dev@users.noreply.github.com>
2024-09-30 16:52:52 +0530

    Count Characters in string

--- codewar/main.py
+++ codewar/main.py
@@ -1,23 +1,15 @@
-def longest_consec(strarr, k):
+def count(s : str ):
+    # The function code should be here
     
-    string_length = ""
-    if k < len(strarr) and k > 0:
-        for string in range(len(strarr)):
-            string_added = strarr[string:string+k]
-            if len(string_added) == k:
-                strin = "".join(string_added)
-                string_length+=strin + ","
-    if k  == 0 or k > len(strarr) or k < 0:
-        return ""
+    count_string = {}
     
-    
-    split_str = string_length.split(',')
-    len_stinng = []
-    for item in split_str:
-        len_stinng.append(len(item))
-    return len_stinng
-
-starr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"]
-k = 0
-
-print(longest_consec(starr, k))
+    for string in s:
+        if string in count_string:
+            count_string[string]+=1
+        else:
+            count_string[string] =1
+            
+    return count_string
+            
+s  = 'aabb'
+print(count(s))
