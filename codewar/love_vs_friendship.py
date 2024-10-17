61213ec0e97fdfdd56b2b1b2a943f2ff92f0d771
ryan <39787691+parven-dev@users.noreply.github.com>
2024-10-17 22:21:05 +0530

    added z

--- codewar/love_vs_friendship.py
+++ codewar/love_vs_friendship.py
@@ -1,21 +1,17 @@
 def words_to_marks(s):
     
     words = {}
-    
-    for x, chars in enumerate(range(97, 122)):
+    for x, chars in enumerate(range(97, 122+1)):
         words[chr(chars)] = x+1
-      
       
     total = 0  
     for word in  s:
         if word in words:
-            valuess = words.get(word)
-            total+=valuess
-            
-        
+            # valuess = words.get(word)
+            # total+=valuess
+            total+=words.get(word)
+               
     return total
-        
     
-    
-s = "attitude"
+s = "cqavtqnrlosxzcupr"
 print(words_to_marks(s))
