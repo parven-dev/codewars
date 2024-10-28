97e757d58627ae12fa5d286180ea6b0986121001
ryan <39787691+parven-dev@users.noreply.github.com>
2024-10-28 17:24:22 +0530

    The vowl count
     encode() to replace all the lowercase vowels in a given string with numbers,
     decode() to turn the numbers back into vowels

---
+++ find_missing_num_in_arr.py/find_missing_num_in_arr.py
@@ -0,0 +1,18 @@
+def find_missing_number(arr):
+    
+    sum_of_all = 0
+    for num in arr:
+        sum_of_all+=num
+    
+    
+    result = 5050 - sum_of_all 
+    return result
+
+
+arr = []
+for item in range(1, 100+1):
+    arr.append(item)
+
+arr.remove(74)
+
+print(find_missing_number(arr))
