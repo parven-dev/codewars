9e3d772cf2bbb9dc5c136a778ec6d5ad5560181d
ryan <39787691+parven-dev@users.noreply.github.com>
2024-11-20 17:19:25 +0530

    added range with len

--- codewar/your_order_please/main.py
+++ codewar/your_order_please/main.py
@@ -1,32 +1,28 @@
 def order(sentence:str) -> str:
   # code here
   
-  string = sentence.replace(" ", "")
-  to_hex = []
-  for item in string:
-    if ord(item )>= 49 and ord(item) <= 59:
-      to_hex.append(chr(ord(item)))
+  # string = sentence.replace(" ", "")
+  # to_hex = []
+  # for item in string:
+  # #   if ord(item )>= 49 and ord(item) <= 59:
+  # #     to_hex.append(chr(ord(item)))
   
   
-  sorted_list = sorted(to_hex)
-  print(sorted_list[0])
+  # # sorted_list = sorted(to_hex)
   
-  for i, items in enumerate(sentence.split(" ")):   
-    string_data =  str(sorted_list[i])
-    print(string_data)
-
-    if string_data in items:
-      print(sorted_list[i])
-    
-    # if sorted_list[items] in sentence[items]:
-      
-    #   print(sentence[items])
-        
-            
-  return sorted_list
-
-
-
+  data = []
+  
+  words = sentence.split(" ")
+  # print(words)
+  for items in range(1, len(words) + 1):
+    # print(items) 
+    index = items - 1
+    # print(words[index])
+    if str(index) in words:
+      # print(str(index))
+      data.append("uess")
+             
+  return data
 
 sentence = "is2 Thi1s T4est 3a"
 print(order(sentence))
